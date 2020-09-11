67a6827b6348e3562ffa276649cc377e14a75309
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-09-11 13:22:43 +0200

    feat(web:footer): Footer for rendering footer content

---
+++ web/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Want something done?</p>
+      <p>SEB</p>
+      <ul>
+        <li>seb.dekker@gmail.com</li>
+        <li>Amsterdam</li>
+        <li>
+          <a href="https://www.linkedin.com/in/sebastiaan-dekker-411184108/">
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
