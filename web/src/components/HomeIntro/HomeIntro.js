3e728f62656dbe4188b2ee7105b08b8547fe81eb
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-09-11 12:10:11 +0200

    feat(web:home): HomeIntro for rendering intro content

---
+++ web/src/components/HomeIntro/HomeIntro.js
@@ -0,0 +1,14 @@
+const HomeIntro = () => {
+  return (
+    <>
+      <h1>SEB</h1>
+      <p>Creating meaningful digital products</p>
+      <p>
+        I’m Sebastiaan Dekker, a Digital Designer & Webdeveloper based in
+        Amsterdam. I craft websites / shops from concept up to the final result.
+      </p>
+    </>
+  )
+}
+
+export default HomeIntro
