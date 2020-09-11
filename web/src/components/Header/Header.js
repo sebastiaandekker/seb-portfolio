cbbaf88641479adab85a86137b44acbc4d1eb32a
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-09-11 13:22:02 +0200

    feat(web:header): Header for rendering logo and menu

---
+++ web/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <header>
+      <div>SEB</div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Work</a>
+          </li>
+          <li>
+            <a href="#">Profile</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
