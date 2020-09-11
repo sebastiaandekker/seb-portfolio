4ee9e0896fda5a96fe0b25e52c734b1e4aa8f08f
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-09-11 11:56:25 +0200

    feat(web:home): generate HomePage, added layout & cell to page, correct route path to home

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import HomeLayout from 'src/layouts/HomeLayout/HomeLayout'
+import HomeProjectListCell from 'src/components/HomeProjectListCell'
+
+const HomePage = () => {
+  return (
+    <HomeLayout>
+      <HomeProjectListCell></HomeProjectListCell>
+    </HomeLayout>
+  )
+}
+
+export default HomePage
