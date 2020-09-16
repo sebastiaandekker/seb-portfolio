5d9ec772a3d3f73a04a64b12fc021639f88fa5e8
Sebastiaan Dekker <sebastiaan@Sebastiaans-MBP.codaisseur.local>
2020-09-16 14:22:57 +0200

    refactor(tailwind): change screens-, fontFamily-, fontSize-defaults into values fromt design

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,6 +2,32 @@ module.exports = {
   purge: [],
   theme: {
     extend: {},
+    screens: {
+      sm: '400px',
+      md: '648px',
+      lg: '1008px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      sans: ['FuturaLight'],
+      sansBook: ['FuturaBook'],
+      sansMedium: ['FuturaMedium'],
+      sansBold: ['FuturaHeavy'],
+      serifRegular: ['MrsEavesRoman'],
+      serifBold: ['MrsEavesBold'],
+      mono: ['LetterGothicStd'],
+    },
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      base: '1rem', // 16px : 18px
+      lg: '1.25rem', // 16px : 18px
+      h5: '1.414rem', // 22.62px : 25.45px
+      h4: '2rem', // 32px : 36px
+      h3: '2.827rem', // 45.23px : 50.89px
+      h2: '3.998rem', // 63.96px : 71.96px
+      h1: '5.653rem', // 90.44px : 101.75px
+    },
   },
   variants: {},
   plugins: [],
